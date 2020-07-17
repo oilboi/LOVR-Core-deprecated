db9039f1313e2b83866cbfc0136bc5830ef3aafc
oilboi <47129783+oilboi@users.noreply.github.com>
2020-07-16 21:38:32 -0400

    Update thread_test to do multi thread addition

--- thread_test/main.lua
+++ thread_test/main.lua
@@ -1,24 +1,37 @@
-function lovr.load()
-    thread_code = [[
+thread_code = [[
         local lovr = { thread = require 'lovr.thread' }
-        local channel = lovr.thread.getChannel("test")
-        local x = 0
+        local channel = lovr.thread.getChannel("send")
+        local channel2 = lovr.thread.getChannel("receive")
         while true do
-            x = x + 1
-            channel:push(x)
+            local x = channel:pop(false)
+            
+            if x then
+                x = x * 2
+                channel2:push(x,false)
+            end
         end
     ]]
 
-    channel = lovr.thread.getChannel("test")
 
-    thread = lovr.thread.newThread(thread_code)
+function lovr.load()    
+    channel = lovr.thread.getChannel("send")
+    channel2 = lovr.thread.getChannel("receive")
 
+    thread = lovr.thread.newThread(thread_code)    
     thread:start()
+
+    message = 1
+
+    channel:push(1)
 end
 
 
 function lovr.update(dt)
-    message = channel:pop()
+    local x = channel2:pop(false)
+    if x then
+        message = x
+        channel:push(x*2,false)
+    end
 end
 
 
