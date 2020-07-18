7115d4d0fe7de96b5381156ad9fa2d19d2ab6874
oilboi <47129783+oilboi@users.noreply.github.com>
2020-07-17 21:59:12 -0400

    update ffi thread test

--- ffi_test/main.lua
+++ ffi_test/main.lua
@@ -8,15 +8,14 @@ end
 function lovr.update()
     lovr.event.pump()
 
-    local blob = lovr.data.newBlob(16*16*128*10)
+    local blob = lovr.data.newBlob(16*16*128*8)
     local array = ffi.cast("double*", blob:getPointer())
     for i = 1,16*16*128 do
-        array[i] = math.random()
+        array[i] = i
+        local lag = array[i]
     end
     io.write("done")
     io.write("\n")
-
-    the_temp_output = array[math.random(1,16*16*128)]
 end
 
 
@@ -24,5 +23,4 @@ end
 function lovr.draw()
     local fps = lovr.timer.getFPS()
     lovr.graphics.print(tostring(fps), 0, 1.7, -5)
-    lovr.graphics.print(tostring(the_temp_output), 0, 0, -5)
 end
--- thread_test/main.lua
+++ thread_test/main.lua
@@ -2,41 +2,23 @@ thread_code = [[
         local lovr = { thread = require 'lovr.thread' }
         local channel = lovr.thread.getChannel("send")
         local channel2 = lovr.thread.getChannel("receive")
-        local channel3 = lovr.thread.getChannel("somethingelse")
         while true do
             local x = channel:pop(false)
             
             if x then
-                x = x + 2
+                x = x * 2
                 channel2:push(x,false)
             end
         end
     ]]
-thread_code2 = [[
-        local lovr = { thread = require 'lovr.thread' }
-        local channel = lovr.thread.getChannel("send")
-        local channel2 = lovr.thread.getChannel("receive")
-        local channel3 = lovr.thread.getChannel("somethingelse")
-        while true do
-            local x = channel2:pop(false)
-            
-            if x then
-                x = x + 2
-                channel3:push(x,false)
-            end
-        end
-    ]]
+
 
 function lovr.load()    
     channel = lovr.thread.getChannel("send")
     channel2 = lovr.thread.getChannel("receive")
-    channel3 = lovr.thread.getChannel("somethingelse")
 
-    thread = lovr.thread.newThread(thread_code)
+    thread = lovr.thread.newThread(thread_code)    
     thread:start()
-
-    thread2 = lovr.thread.newThread(thread_code2)
-    thread2:start()
 
     message = 1
 
@@ -45,10 +27,10 @@ end
 
 
 function lovr.update(dt)
-    local x = channel3:pop(false)
+    local x = channel2:pop(false)
     if x then
         message = x
-        channel:push(x + 2,false)
+        channel:push(x*2,false)
     end
 end
 
